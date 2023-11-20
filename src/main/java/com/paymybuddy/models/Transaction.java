b2ae8370eb8a0836c5a89dc78fab8ae2cdb69b1e
cariocaphil <cariocaphil@gmail.com>
2023-11-20 15:45:16 +0100

    add getters and setters with lombok

--- src/main/java/com/paymybuddy/models/Transaction.java
+++ src/main/java/com/paymybuddy/models/Transaction.java
@@ -1,5 +1,18 @@
 package com.paymybuddy.models;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
   private long transactionID;
   private double amount;
--- src/main/java/com/paymybuddy/models/User.java
+++ src/main/java/com/paymybuddy/models/User.java
@@ -1,7 +1,19 @@
 package com.paymybuddy.models;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
   private long userID;
   private String email;

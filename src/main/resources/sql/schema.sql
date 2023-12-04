fbf5371543221a64c47cfa2543d276f73f322f3a
cariocaphil <cariocaphil@gmail.com>
2023-12-04 22:06:37 +0100

    add sql scripts

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_table (
+  userid BIGINT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL,
+  socialmediaacc VARCHAR(50) NOT NULL,
+  balance DOUBLE PRECISION NOT NULL
+);
+CREATE INDEX idx_email ON user_table (email);

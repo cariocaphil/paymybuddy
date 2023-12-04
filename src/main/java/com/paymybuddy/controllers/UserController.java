32c4978c26c36576ee001510d788336967fed06f
cariocaphil <cariocaphil@gmail.com>
2023-12-04 22:28:05 +0100

    add retrieve user by id

--- src/main/java/com/paymybuddy/controllers/UserController.java
+++ src/main/java/com/paymybuddy/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class UserController {
   @GetMapping
   public List<User> getAllUsers(){
     return userService.getAllUsers();
+  }
+
+  @GetMapping("/{userId}")
+  public User getUserById(@PathVariable long userId) {
+    return userService.getUserById(userId);
   }
 }

118028b4ebef1c3522887f5a5575d14dfdf314e9
cariocaphil <cariocaphil@gmail.com>
2024-02-25 21:30:08 +0100

    update schema regarding transaction table

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -7,3 +7,18 @@ CREATE TABLE user_table (
 );
 
 CREATE INDEX idx_email ON user_table (email);
+
+CREATE TABLE transaction_table (
+  transactionid BIGINT PRIMARY KEY,
+  amount DOUBLE PRECISION NOT NULL,
+  timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  description TEXT,
+  fee DOUBLE PRECISION NOT NULL,
+  sender_userid BIGINT NOT NULL,
+  receiver_userid BIGINT NOT NULL,
+  FOREIGN KEY (sender_userid) REFERENCES user_table(userid),
+  FOREIGN KEY (receiver_userid) REFERENCES user_table(userid)
+);
+
+CREATE INDEX idx_sender_userid ON transaction_table (sender_userid);
+CREATE INDEX idx_receiver_userid ON transaction_table (receiver_userid);
